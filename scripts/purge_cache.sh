05a9672ed36a5474347ccf813bb298f5654a6938
nyarla <nyarla@kalaclista.com>
2021-07-04 13:16:15 +0900

    bugfix: fix wrong header key

--- scripts/purge_cache.sh
+++ scripts/purge_cache.sh
@@ -8,10 +8,12 @@ fi
 
 if test $(cat resources/_gen/purge.txt | wc -l) -gt 30 ; then
   curl -X POST "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/purge_cache" \
-    -H "Authentication: Bearer ${ZONE_TOKEN}" \
+    -H "Authorization: Bearer ${ZONE_TOKEN}" \
+    -H "Content-Type: application/json" \
     --data '{"purge_everything":true}'
 else
   curl -X POST "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/purge_cache" \
-    -H "Authentication: Bearer ${ZONE_TOKEN}" \
+    -H "Authorization: Bearer ${ZONE_TOKEN}" \
+    -H "Content-Type: application/json" \
     --data "$(cat resources/_gen/purge.txt | sed 's/^\(.\+\)$/"\1",/g' | tr "\n" " " | (echo -n '{"files":['; cat - ; echo -n "]}") | sed 's/, ]}$/]}/')"
 fi
