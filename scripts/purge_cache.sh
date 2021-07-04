8a0e3a64bdc7599f22561c06fb1567cefc930d20
nyarla <nyarla@kalaclista.com>
2021-07-04 13:58:11 +0900

    bugfix: use full length URL

--- scripts/purge_cache.sh
+++ scripts/purge_cache.sh
@@ -15,5 +15,5 @@ else
   curl -X POST "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/purge_cache" \
     -H "Authorization: Bearer ${ZONE_TOKEN}" \
     -H "Content-Type: application/json" \
-    --data "$(cat resources/_gen/purge.txt | sed 's/^\(.\+\)$/"\1",/g' | tr "\n" " " | (echo -n '{"files":['; cat - ; echo -n "]}") | sed 's/, ]}$/]}/')"
+    --data "$(cat resources/_gen/purge.txt | sed 's/^\(.\+\)$/"https:\/\/the.kalaclista.com\/\1",/g' | tr "\n" " " | (echo -n '{"files":['; cat - ; echo -n "]}") | sed 's/, ]}$/]}/')"
 fi
